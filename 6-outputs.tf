de0caf0c96e62b40a393159c0324514d6cab782b
Jesse-home-linux <jesse.islam@gmail.com>
2025-07-25 09:37:11 -0400

    commented out terraform blocks that should only run first time

--- 6-outputs.tf
+++ 6-outputs.tf
@@ -1,30 +1,30 @@
-resource "google_api_gateway_api" "app" {
-  provider = google-beta
-  api_id   = "app-api"
-}
+#resource "google_api_gateway_api" "app" {
+#  provider = google-beta
+#  api_id   = "app-api"
+#}
 
-resource "google_api_gateway_api_config" "app" {
-  provider      = google-beta
-  api           = google_api_gateway_api.app.api_id
-  api_config_id = "v1"
+#resource "google_api_gateway_api_config" "app" {
+#  provider      = google-beta
+#  api           = google_api_gateway_api.app.api_id
+#  api_config_id = "v1"
 
-  openapi_documents {
-    document {
-      # This is just the "logical" name of the file in your Gateway.
-      path     = "openapi.yaml"
+#  openapi_documents {
+#    document {
+#      # This is just the "logical" name of the file in your Gateway.
+#      path     = "openapi.yaml"
+#
+#      # These are the actual bytes of your spec, base‑64 encoded.
+#      contents = filebase64("${path.module}/openapi.yaml")
+#    }
+#  }
+#}
 
-      # These are the actual bytes of your spec, base‑64 encoded.
-      contents = filebase64("${path.module}/openapi.yaml")
-    }
-  }
-}
 
-
-resource "google_api_gateway_gateway" "app" {
-  provider   = google-beta
-  gateway_id = "app-gateway"
-  api_config = google_api_gateway_api_config.app.name
-}
+#resource "google_api_gateway_gateway" "app" {
+#  provider   = google-beta
+#  gateway_id = "app-gateway"
+#  api_config = google_api_gateway_api_config.app.name
+#}
 
 output "run_url" {
   value = google_cloud_run_service.python_service.status[0].url
